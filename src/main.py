0d7d53dfea4cc284bca30964de046fd48ab6c2e6
Mateus-Alencar <mateus.alencar3182@outlook.com>
2026-03-07 15:16:52 -0300

    Adicionado escopo do main.py

---
+++ src/main.py
@@ -0,0 +1,18 @@
+class Grafo:
+  def __init__(self, vertices):
+    self.vertices = vertices
+    
+print("--------------------------------------------------------------------")
+print("Implementação de matriz de adjacências para grafos e dígrafos.")
+vertices = input("Informe as arestas do grafo (origem destino,origem destino, ...): ")
+print("----------------------------- Execução -----------------------------")
+print("Matriz de Adjacências para Grafo:")
+print("""
+V |
+-------
+""")
+print("Matriz de Adjacências para Dígrafo:")
+print("""
+V |
+-------
+""")
