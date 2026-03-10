9305a42b2b1be676c9e63ce6aae9d8b3745a7cb6
Mateus-Alencar <mateus.alencar3182@outlook.com>
2026-03-09 23:04:10 -0300

    Montando Dicionario do Grafo

--- src/main.py
+++ src/main.py
@@ -1,20 +1,46 @@
 class Grafo:
   def __init__(self, vertices):
     self.vertices = vertices
+    self.lista_adjacente = []
 
   def montar_lista_adjacências(self):
-    resultado = []
     # Separar cada par usando a vírgula.
     for par in self.vertices.split(","):
       # par.strip() -> Remove espaços extras no começo e fim da string
       # .split() -> divide a string em uma lista usando o espaço como separador
       # map(int) aplica a função int() em cada elemento da lista
       a, b = map(int, par.strip().split())
-      resultado.append({a: b})
+      self.lista_adjacente.append({a: b})
+    print(self.lista_adjacente)
+    self.montarGrafo()
+    self.montarDigrafo()
+
   def montarGrafo(self):
-    pass
+    lista = self.lista_adjacente
+    DicionarioGrafo = {}
+    if len(self.lista_adjacente) != 0:
+      if lista:
+        todos_numeros = []
+        for num in lista:
+          for chave, valor in num.items():
+            # extend() -> serve para adicionar múltiplos elementos a uma lista
+            todos_numeros.extend([chave, valor])
+        maior_vertice = max(todos_numeros)
+      print(maior_vertice)
+      for x in range(1, maior_vertice):
+        DicionarioGrafo[x] = []
+      print(DicionarioGrafo)
+
+    else:
+      print("A Lista está vazia")
+
   def montarDigrafo(self):
+    if len(self.lista_adjacente) != 0:
+      pass
+    else:
+      print("A Lista está vazia")
     pass
+
 print("--------------------------------------------------------------------")
 print("Implementação de matriz de adjacências para grafos e dígrafos.")
 vertices = input("Informe as arestas do grafo (origem destino,origem destino, ...): ")
